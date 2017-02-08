0a3012abf0e8494a19780d95360fe1b2579bc7fe
bernard <bernardkarunaweera@gmail.com>
2017-02-08 21:03:28 +0800

    Updates

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,17 +7,35 @@
   function LunchCheckController($scope) {
     $scope.items = "";
     $scope.message = "";
+    $scope.fontColor = "";
     $scope.checktooMuch = function () {
+
+      // Check for the Number of items
       var itemArray = $scope.items.split(",");
+      // Check for Empty items
+      var cloneItemArray = [];
+      for (var i = 0; i < itemArray.length; i++) {
+        if(itemArray[i].trim() != ""){
+          cloneItemArray.push(itemArray[i]);
+        }
+      }
+
       var displayMessage = "";
-      if(itemArray.length > 0 && itemArray.length <= 3){
+      var fontVal = "";
+      // Check if array is empty
+      if(cloneItemArray.length == 0){
+        displayMessage = "Please enter data first";
+        fontVal = "red";
+      }
+      else if(cloneItemArray.length <= 3){
         displayMessage = "Enjoy";
+        fontVal = "green";
       }
-
-      if(itemArray.length > 3){
+      else if(cloneItemArray.length > 3){
         displayMessage = "Too much!";
+        fontVal = "green";
       }
-
+      $scope.fontColor = fontVal;
       $scope.message = displayMessage;
     };
   }
